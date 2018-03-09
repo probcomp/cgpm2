6f245abb26e745b8f1d5fb99a8e89bfc5dd22717
Feras A Saad <fsaad@mit.edu>
2018-03-09 13:17:18 -0500

    Fix bug for list versus dict return type, with assertions.

--- src/finite_rowmix.py
+++ src/finite_rowmix.py
@@ -3,6 +3,8 @@
 # Copyright (c) 2018 MIT Probabilistic Computing Project.
 # Released under Apache 2.0; refer to LICENSE.txt.
 
+import itertools
+
 import numpy as np
 
 from cgpm.utils.general import build_cgpm
@@ -61,7 +63,13 @@ class FiniteRowMixture(CGPM):
             self._simulate_one(rowid, targets, constraints, inputs, n, z)
             for z, n in counts.iteritems()
         ]
-        return samples[0] if N is None else lchain(*samples)
+        if N is None:
+            assert len(samples) == 1
+            assert len(samples[0]) == 1
+            assert len(samples[0][0]) == len(targets)
+            return samples[0][0]
+        else:
+            return list(itertools.chain.from_iterable(samples))
 
     def logpdf(self, rowid, targets, constraints=None, inputs=None):
         if rowid in self.rowid_to_component:
